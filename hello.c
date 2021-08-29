e5fc8bdbc24a30fff9cd68c0fd74493fbcd09c22
Michael Endacott <mendacott3@huskers.unl.edu>
2021-08-29 21:57:53 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Michael Endacott, Colby Chapelle 
+ *
+ *         Mendacott3@huskers.unl.edu
+ * Date: 2021/29/08
+ *
+ * 
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Major: Computer Science\n");
+
+  return 0;
+}
+
